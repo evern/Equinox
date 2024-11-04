000f23ae55ae6c61db7c1ae79acc10e7ebd85b30
evern <evern@outlook.com>
2024-11-04 16:25:22 +0800

    change login screen logo

--- CheckmateDX/frmLogin.Designer.cs
+++ CheckmateDX/frmLogin.Designer.cs
@@ -35,12 +35,12 @@
             this.txtPassword = new DevExpress.XtraEditors.TextEdit();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.cbDefault = new DevExpress.XtraEditors.CheckEdit();
-            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.pnlBottom = new DevExpress.XtraEditors.PanelControl();
             this.btnOk = new DevExpress.XtraEditors.SimpleButton();
             this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
             this.pnlHeader = new DevExpress.XtraEditors.PanelControl();
             this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl4)).BeginInit();
             this.panelControl4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).BeginInit();
@@ -54,17 +54,17 @@
             // 
             // panelControl4
             // 
+            this.panelControl4.Controls.Add(this.txtPassword);
+            this.panelControl4.Controls.Add(this.labelControl2);
             this.panelControl4.Controls.Add(this.lblVersion);
             this.panelControl4.Controls.Add(this.txtUsername);
-            this.panelControl4.Controls.Add(this.txtPassword);
             this.panelControl4.Controls.Add(this.labelControl1);
             this.panelControl4.Controls.Add(this.cbDefault);
-            this.panelControl4.Controls.Add(this.labelControl2);
             this.panelControl4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelControl4.Location = new System.Drawing.Point(0, 105);
-            this.panelControl4.Margin = new System.Windows.Forms.Padding(4);
+            this.panelControl4.Location = new System.Drawing.Point(0, 158);
+            this.panelControl4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.panelControl4.Name = "panelControl4";
-            this.panelControl4.Size = new System.Drawing.Size(568, 382);
+            this.panelControl4.Size = new System.Drawing.Size(568, 322);
             this.panelControl4.TabIndex = 0;
             // 
             // lblVersion
@@ -75,33 +75,33 @@
             this.lblVersion.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
             this.lblVersion.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
             this.lblVersion.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
-            this.lblVersion.Location = new System.Drawing.Point(334, 340);
-            this.lblVersion.Margin = new System.Windows.Forms.Padding(4);
+            this.lblVersion.Location = new System.Drawing.Point(501, 510);
+            this.lblVersion.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(189, 33);
+            this.lblVersion.Size = new System.Drawing.Size(284, 50);
             this.lblVersion.TabIndex = 8;
             this.lblVersion.Text = "Release 0.0.0.0";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(148, 44);
-            this.txtUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUsername.Location = new System.Drawing.Point(219, 69);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Properties.Appearance.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtUsername.Properties.Appearance.Options.UseFont = true;
-            this.txtUsername.Size = new System.Drawing.Size(375, 30);
+            this.txtUsername.Size = new System.Drawing.Size(282, 30);
             this.txtUsername.TabIndex = 0;
             this.txtUsername.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUsername_KeyDown);
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(148, 112);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Location = new System.Drawing.Point(219, 150);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Properties.Appearance.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPassword.Properties.Appearance.Options.UseFont = true;
             this.txtPassword.Properties.UseSystemPasswordChar = true;
-            this.txtPassword.Size = new System.Drawing.Size(375, 30);
+            this.txtPassword.Size = new System.Drawing.Size(282, 30);
             this.txtPassword.TabIndex = 1;
             this.txtPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPassword_KeyDown);
             // 
@@ -110,58 +110,42 @@
             this.labelControl1.Appearance.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelControl1.Appearance.Options.UseFont = true;
             this.labelControl1.Appearance.Options.UseTextOptions = true;
-            this.labelControl1.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
+            this.labelControl1.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Near;
             this.labelControl1.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Top;
             this.labelControl1.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
-            this.labelControl1.Location = new System.Drawing.Point(8, 48);
-            this.labelControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.labelControl1.Location = new System.Drawing.Point(78, 69);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Padding = new System.Windows.Forms.Padding(0, 0, 30, 0);
-            this.labelControl1.Size = new System.Drawing.Size(150, 60);
+            this.labelControl1.Padding = new System.Windows.Forms.Padding(0, 0, 45, 0);
+            this.labelControl1.Size = new System.Drawing.Size(129, 30);
             this.labelControl1.TabIndex = 5;
             this.labelControl1.Text = "Username";
             // 
             // cbDefault
             // 
-            this.cbDefault.Location = new System.Drawing.Point(344, 170);
-            this.cbDefault.Margin = new System.Windows.Forms.Padding(4);
+            this.cbDefault.Location = new System.Drawing.Point(231, 221);
+            this.cbDefault.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.cbDefault.Name = "cbDefault";
             this.cbDefault.Properties.Appearance.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbDefault.Properties.Appearance.Options.UseFont = true;
             this.cbDefault.Properties.Caption = "Select Defaults";
             this.cbDefault.Properties.CheckStyle = DevExpress.XtraEditors.Controls.CheckStyles.Style10;
             this.cbDefault.Properties.GlyphAlignment = DevExpress.Utils.HorzAlignment.Far;
-            this.cbDefault.Size = new System.Drawing.Size(180, 28);
+            this.cbDefault.Size = new System.Drawing.Size(270, 28);
             this.cbDefault.TabIndex = 2;
             this.cbDefault.CheckedChanged += new System.EventHandler(this.cbDefault_CheckedChanged);
             this.cbDefault.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cbDefault_KeyDown);
-            // 
-            // labelControl2
-            // 
-            this.labelControl2.Appearance.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelControl2.Appearance.Options.UseFont = true;
-            this.labelControl2.Appearance.Options.UseTextOptions = true;
-            this.labelControl2.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
-            this.labelControl2.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Top;
-            this.labelControl2.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
-            this.labelControl2.Location = new System.Drawing.Point(0, 117);
-            this.labelControl2.Margin = new System.Windows.Forms.Padding(4);
-            this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Padding = new System.Windows.Forms.Padding(0, 0, 30, 0);
-            this.labelControl2.Size = new System.Drawing.Size(150, 60);
-            this.labelControl2.TabIndex = 7;
-            this.labelControl2.Text = "Password";
             // 
             // pnlBottom
             // 
             this.pnlBottom.Controls.Add(this.btnOk);
             this.pnlBottom.Controls.Add(this.btnCancel);
             this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlBottom.Location = new System.Drawing.Point(0, 487);
-            this.pnlBottom.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlBottom.Location = new System.Drawing.Point(0, 480);
+            this.pnlBottom.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pnlBottom.Name = "pnlBottom";
-            this.pnlBottom.Padding = new System.Windows.Forms.Padding(4);
-            this.pnlBottom.Size = new System.Drawing.Size(568, 75);
+            this.pnlBottom.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pnlBottom.Size = new System.Drawing.Size(568, 82);
             this.pnlBottom.TabIndex = 1;
             // 
             // btnOk
@@ -169,12 +153,12 @@
             this.btnOk.Appearance.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOk.Appearance.Options.UseFont = true;
             this.btnOk.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnOk.ImageOptions.Image")));
-            this.btnOk.Location = new System.Drawing.Point(18, 9);
-            this.btnOk.LookAndFeel.SkinName = "Visual Studio 2013 Light";
+            this.btnOk.Location = new System.Drawing.Point(15, 14);
+            this.btnOk.LookAndFeel.SkinName = "Office 2019 Dark Gray";
             this.btnOk.LookAndFeel.UseDefaultLookAndFeel = false;
-            this.btnOk.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(150, 60);
+            this.btnOk.Size = new System.Drawing.Size(160, 50);
             this.btnOk.TabIndex = 0;
             this.btnOk.Text = "&Login";
             this.btnOk.Click += new System.EventHandler(this.btnLogin_Click);
@@ -184,32 +168,32 @@
             this.btnCancel.Appearance.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancel.Appearance.Options.UseFont = true;
             this.btnCancel.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnCancel.ImageOptions.Image")));
-            this.btnCancel.Location = new System.Drawing.Point(400, 9);
-            this.btnCancel.LookAndFeel.SkinName = "Visual Studio 2013 Light";
+            this.btnCancel.Location = new System.Drawing.Point(393, 12);
+            this.btnCancel.LookAndFeel.SkinName = "Office 2019 Dark Gray";
             this.btnCancel.LookAndFeel.UseDefaultLookAndFeel = false;
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Padding = new System.Windows.Forms.Padding(4);
-            this.btnCancel.Size = new System.Drawing.Size(150, 60);
+            this.btnCancel.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnCancel.Size = new System.Drawing.Size(160, 50);
             this.btnCancel.TabIndex = 1;
             this.btnCancel.Text = "&Cancel";
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // pnlHeader
             // 
-            this.pnlHeader.Appearance.BackColor = System.Drawing.Color.Black;
+            this.pnlHeader.Appearance.BackColor = System.Drawing.Color.WhiteSmoke;
             this.pnlHeader.Appearance.Options.UseBackColor = true;
             this.pnlHeader.Controls.Add(this.labelControl3);
             this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlHeader.Location = new System.Drawing.Point(0, 0);
             this.pnlHeader.Margin = new System.Windows.Forms.Padding(0);
             this.pnlHeader.Name = "pnlHeader";
-            this.pnlHeader.Size = new System.Drawing.Size(568, 105);
+            this.pnlHeader.Size = new System.Drawing.Size(568, 158);
             this.pnlHeader.TabIndex = 99;
             // 
             // labelControl3
             // 
-            this.labelControl3.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.labelControl3.Appearance.BackColor = System.Drawing.Color.DarkGray;
             this.labelControl3.Appearance.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelControl3.Appearance.Options.UseBackColor = true;
             this.labelControl3.Appearance.Options.UseFont = true;
@@ -223,8 +207,24 @@
             this.labelControl3.Location = new System.Drawing.Point(2, 2);
             this.labelControl3.Margin = new System.Windows.Forms.Padding(0);
             this.labelControl3.Name = "labelControl3";
-            this.labelControl3.Size = new System.Drawing.Size(564, 101);
+            this.labelControl3.Size = new System.Drawing.Size(564, 154);
             this.labelControl3.TabIndex = 99;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Appearance.Options.UseTextOptions = true;
+            this.labelControl2.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Near;
+            this.labelControl2.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Top;
+            this.labelControl2.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl2.Location = new System.Drawing.Point(78, 153);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(9, 9, 9, 9);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Padding = new System.Windows.Forms.Padding(0, 0, 68, 0);
+            this.labelControl2.Size = new System.Drawing.Size(151, 45);
+            this.labelControl2.TabIndex = 9;
+            this.labelControl2.Text = "Password";
             // 
             // frmLogin
             // 
@@ -261,7 +261,6 @@
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.TextEdit txtUsername;
         private DevExpress.XtraEditors.TextEdit txtPassword;
-        private DevExpress.XtraEditors.LabelControl labelControl2;
         private DevExpress.XtraEditors.CheckEdit cbDefault;
         private DevExpress.XtraEditors.SimpleButton btnCancel;
         private DevExpress.XtraEditors.SimpleButton btnOk;
@@ -269,6 +268,6 @@
         private DevExpress.XtraEditors.PanelControl panelControl4;
         private DevExpress.XtraEditors.LabelControl labelControl3;
         private DevExpress.XtraEditors.LabelControl lblVersion;
-
+        private DevExpress.XtraEditors.LabelControl labelControl2;
     }
 }
